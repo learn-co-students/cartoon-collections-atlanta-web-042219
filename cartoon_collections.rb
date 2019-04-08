6e4c83e1517571c05da8fa7ad0de5d22cb236001
han xu <hanxu27@gmail.com>
2019-04-08 23:21:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  arr.find { |item| item == cheese_types }
+  arr.find { |item| item == cheese_types[0] }
 end
