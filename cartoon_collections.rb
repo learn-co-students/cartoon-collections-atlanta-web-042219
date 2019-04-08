a7dceba2f1f28b83db0055ef2d1ad2a71c4a6feb
Andrew Allen <asallen67@gmail.com>
2019-04-08 00:39:32 -0500

    1

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+	for k in 0..arr.size-1 do
+		puts "/#{k+1}.*#{arr[k]}/"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+	arr.collect {|n| n.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+	arr.any? {|n| n.length>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find {|n| n=="cheddar" || n=="gouda" || n=="camembert" }
 end
