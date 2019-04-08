a82b6bff8716452206e61dba44c1bbe9eeb678e7
han xu <hanxu27@gmail.com>
2019-04-08 23:10:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  arr.include?(cheese_types)
 end
