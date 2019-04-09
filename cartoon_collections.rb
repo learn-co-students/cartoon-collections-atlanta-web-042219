89caf2831ca753b297b0f4dcab5ae1c3f2b6957d
han xu <hanxu27@gmail.com>
2019-04-09 00:21:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(arr)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < 3
-    arr.find { |item| item == cheese_types[i] } == nil ? i += 1 : cheese_types[i]
+    if arr.find { |item| item == cheese_types[i] } == nil i += 1 : return cheese_types[i]
   end 
  nil
 end
